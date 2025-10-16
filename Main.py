60c66ab331d1899af4f13dd985431856c1b7dc77
mohamedpelea23-dev <mohamedpelea23@gmail.com>
2025-10-16 18:28:56 -0500

    temp: guardar mis cambios locales antes de actualizar

---
+++ Main.py
@@ -0,0 +1,7 @@
+#======================================================
+# Proyecto: Gestión de Clientes (Aplicando SOLID)
+# Integrantes del grupo:
+#   - Sebastian Muñoz
+#   - Luna Maria 
+#   - Santiago Remolina
+# ======================================================
