f0fc2d5fabf3f6c481dd4db380e2772601316763
mohamedpelea23-dev <mohamedpelea23@gmail.com>
2025-10-16 20:28:10 -0500

    refactor:(main): actualizacion main para resultado final

--- Main.py
+++ Main.py
@@ -6,10 +6,8 @@
 #   - Santiago Remolina
 # ======================================================
 
-def main():
-    print("¡Hola, mundo!")
-    print("Este es un ejemplo básico de main.py")
-
 if __name__ == "__main__":
-    main()
-
+    repo = RepositorioCliente()
+    servicio = ServicioRegistroCliente(repo)
+    cliente1 = Cliente("Andrea", "andrea@empresa.com")
+    servicio.registrar_cliente(cliente1)
