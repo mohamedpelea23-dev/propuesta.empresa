6c69db5f109bddedb803c414d58015ceb875409c
mohamedpelea23-dev <mohamedpelea23@gmail.com>
2025-10-16 19:59:57 -0500

    fix(repositorio): eliminacion de guias visuales

--- repositories.py
+++ repositories.py
@@ -1,13 +1,7 @@
 class RepositorioCliente:
-    """
-    Clase encargada de manejar la persistencia de datos del cliente.
-    (Integrante 3 - Repositorio)
-    """
-
-    def guardar(self, cliente):
-        # Simulación del guardado en una base de datos
-        print(f"✅ Cliente '{cliente}' guardado exitosamente en la base de datos.")
+     
+     def guardar(self, cliente):
+        print(f" Cliente '{cliente}' guardado exitosamente en la base de datos.")
 
     def eliminar(self, cliente):
-        # Simulación de la eliminación del cliente
-        print(f"🗑️ Cliente '{cliente}' eliminado del registro.")
+        print(f" Cliente '{cliente}' eliminado del registro.")
